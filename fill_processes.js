59d57ecc851687d6ac324f30ccb01af7b98557e4
Roberto Soares <roberto.tech@gmail.com>
2015-10-07 21:27:01 -0300

    @roberto / @anikarni - Adds first process number to input field

--- fill_processes.js
+++ fill_processes.js
@@ -1,1 +1,8 @@
-console.log('hello word!');
+var processNumbers = ['00151400106370'];
+var iframe = document.getElementById('iframeBusca');
+
+iframe.onload = function() {
+  var iframeDocument = iframe.contentWindow.document;
+  var processInput = iframeDocument.getElementById('num_processo_mask');
+  processInput.setAttribute('value', processNumbers[0]);
+};
