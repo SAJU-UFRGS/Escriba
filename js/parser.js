b52a8671db1a8727ef7b87c22aed779724036ad4
Pedro Leal <pedrombl@gmail.com>
2015-10-14 20:54:16 -0300

    parsing list of processes that are separated by comma

--- js/parser.js
+++ js/parser.js
@@ -1,3 +1,3 @@
 function parseToListOfProcesses(value) {
-  return [value];
+  return value.split(',');
 }
--- spec/parserSpec.js
+++ spec/parserSpec.js
@@ -2,6 +2,9 @@ describe('Parser', function () {
   it('should parse one id to a list of one id', function() {
     expect(parseToListOfProcesses("1234")).toEqual(["1234"]);
   });
+  it('should parse a string with a list of strings separate by comma', function() {
+    expect(parseToListOfProcesses("1,2,3,4,5")).toEqual(["1", "2", "3", "4", "5"]);
+  });
 });
 
 
