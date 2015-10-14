747eaa5e81a5db7fb01c4bb0be0e29f37947624e
anikarni <anikarni@gmail.com>
2015-10-14 15:03:35 -0300

    Removes process from static array once process info page is opened

--- fill_processes.js
+++ fill_processes.js
@@ -3,6 +3,16 @@ var iframe = document.getElementById('iframeBusca');
 
 iframe.onload = function() {
   var iframeDocument = iframe.contentWindow.document;
+
   var processInput = iframeDocument.getElementById('num_processo_mask');
-  processInput.setAttribute('value', processNumbers[0]);
+  var processInfo = iframeDocument.getElementById('conteudo');
+
+  if (processInput) {
+    processInput.setAttribute('value', processNumbers[0]);
+  } else if (processInfo) {
+    var processURI = processInfo.getElementsByTagName('table')[0].firstChild.baseURI;
+    if (processURI.indexOf(processNumbers[0]) > -1) {
+      processNumbers.splice(0, 1);
+    }
+  }
 };
