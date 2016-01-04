c33c38102813da147df698894c5e067a5c8883e0
Roberto Soares <roberto.tech@gmail.com>
2016-01-04 17:18:47 -0200

    adds feature flag showSidebar

--- js/pageCrawler.js
+++ js/pageCrawler.js
@@ -2,8 +2,10 @@ var iframe;
 iframe = document.getElementById('iframeBusca');
 
 iframe.onload = function() {
-  Sidebar.windowDocument = document;
-  Sidebar.toggleSidebar({open: true});
+  if (Features.showSidebar) {
+    Sidebar.windowDocument = document;
+    Sidebar.toggleSidebar({open: true});
+  }
 
   EscribaHelper.iframeDocument = iframe.contentWindow.document;
   EscribaHelper.updateProcessForPage();
