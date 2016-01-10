a5083dbd1189b1aad28df487406195e399a6bebe
anikarni <anikarni@gmail.com>
2016-01-10 20:01:55 -0300

    Anike - Refactors sidebar creation and stylizes the toggle button

--- js/Sidebar.js
+++ js/Sidebar.js
@@ -1,30 +1,43 @@
 var Sidebar = {
   windowDocument: {},
 
-  setUp: function() {
+  _createSidebar: function() {
     var sidebar = this.windowDocument.createElement('div');
     sidebar.id = "sidebar";
     sidebar.className = "pure-menu custom-restricted-width";
     sidebar.innerHTML = "\
       <span class=\"pure-menu-heading\">Movimentações Recentes</span>\
-      <span class=\"toggle-button\">+</span>\
+      <span id=\"toggle-button\" class=\"pure-button\">+</span>\
       <ul class=\"pure-menu-list\">\
       <ul>\
       ";
     this.windowDocument.body.appendChild(sidebar);
-    sidebar.addEventListener('click', function() {
+  },
+
+  _setUpToggleButton: function() {
+    var button = this.windowDocument.getElementById('toggle-button');
+    button.addEventListener('click', function() {
       Sidebar.toggleSidebar();
     });
+  },
 
+  _slideContent: function(withSidebar) {
+    var leftMargin = withSidebar ? "22%" : "auto";
     var content = this.windowDocument.getElementById('conteudo');
-    content.style.marginLeft = "22%";
+    content.style.marginLeft = leftMargin;
+  },
+
+  setUp: function() {
+    this._createSidebar();
+    this._setUpToggleButton();
+    this._slideContent(true);
   },
 
   toggleSidebar: function(options) {
     options = options || {}
     var el = this.windowDocument.getElementById('sidebar');
     el.className += " hidden";
-    var content = this.windowDocument.getElementById('conteudo');
-    content.style.marginLeft = "auto";
+
+    this._slideContent(false);
   }
 }
