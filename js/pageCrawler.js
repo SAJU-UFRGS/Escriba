3a954822c4d8063627b181ca034befacc576a29f
anikarni <anikarni@gmail.com>
2016-01-10 21:49:02 -0300

    Fixes sidebar so it only creates a new one the first time

--- js/pageCrawler.js
+++ js/pageCrawler.js
@@ -4,7 +4,10 @@ iframe = document.getElementById('iframeBusca');
 iframe.onload = function() {
   if (Features.showSidebar) {
     Sidebar.windowDocument = document;
-    Sidebar.setUp();
+    var sidebar = document.getElementById('sidebar');
+    if (sidebar === null) {
+      Sidebar.setUp();
+    }
   }
 
   EscribaHelper.iframeDocument = iframe.contentWindow.document;
