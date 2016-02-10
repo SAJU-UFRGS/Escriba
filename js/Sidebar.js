24f95348a80358b1fd6f50c332cce1b403d3b3b7
anikarni <anikarni@gmail.com>
2016-02-10 20:28:22 -0200

    Moves up public methods in Sidebar

--- js/Sidebar.js
+++ js/Sidebar.js
@@ -2,6 +2,36 @@ var Sidebar = {
   windowDocument: {},
   sidebarOpen: false,
 
+  setUp: function() {
+    this._createSidebar();
+    this._setUpToggleButton();
+    this._slideContent();
+    this._addAllUpdates();
+  },
+
+  toggleSidebar: function() {
+    var el = this.windowDocument.getElementById('sidebar-content');
+
+    if (this.sidebarOpen) {
+      el.className += " hidden";
+      this.sidebarOpen = false;
+    } else {
+      el.className = el.className.replace(" hidden", "");
+      this.sidebarOpen = true;
+    }
+
+    this._slideContent();
+    this._slideButton();
+  },
+
+  addUpdates: function(processNumber, processUpdates) {
+    var el = this.windowDocument.getElementById('updates-list');
+    el.innerHTML += "<li class=\"pure-menu-heading\">" + processNumber + "</li>";
+    processUpdates.forEach(function(update) {
+      el.innerHTML += "<li class=\"pure-menu-item\">" + update.date + ": " + update.update + "</li>";
+    });
+  },
+
   _createSidebar: function() {
     var sidebar = this.windowDocument.createElement('div');
     sidebar.id = "sidebar";
@@ -35,36 +65,6 @@ var Sidebar = {
     var leftMargin = this.sidebarOpen ? "18%" : "0%";
     var button = this.windowDocument.getElementById('toggle-button');
     button.style.left = leftMargin;
-  },
-
-  setUp: function() {
-    this._createSidebar();
-    this._setUpToggleButton();
-    this._slideContent();
-    this._addAllUpdates();
-  },
-
-  toggleSidebar: function() {
-    var el = this.windowDocument.getElementById('sidebar-content');
-
-    if (this.sidebarOpen) {
-      el.className += " hidden";
-      this.sidebarOpen = false;
-    } else {
-      el.className = el.className.replace(" hidden", "");
-      this.sidebarOpen = true;
-    }
-
-    this._slideContent();
-    this._slideButton();
-  },
-
-  addUpdates: function(processNumber, processUpdates) {
-    var el = this.windowDocument.getElementById('updates-list');
-    el.innerHTML += "<li class=\"pure-menu-heading\">" + processNumber + "</li>";
-    processUpdates.forEach(function(update) {
-      el.innerHTML += "<li class=\"pure-menu-item\">" + update.date + ": " + update.update + "</li>";
-    });
   },
 
   _addAllUpdates: function() {
