4f8de72b5eac96bc1a07e504126f1b5972ba8535
anikarni <anikarni@gmail.com>
2016-03-02 20:06:12 -0300

    Readds sidebar and css

--- js/pageCrawler.js
+++ js/pageCrawler.js
@@ -1,13 +1,23 @@
-var iframe;
+var iframe, style;
+
+var insertCSS = function(file) {
+  style = document.createElement('link');
+  style.rel = 'stylesheet';
+  style.type = 'text/css';
+  style.href = chrome.extension.getURL(file);
+  (document.head||document.documentElement).appendChild(style);
+}
+
+insertCSS('css/index.css');
+insertCSS('node_modules/purecss/build/pure-min.css');
+
 iframe = document.getElementById('iframeBusca');
 
 iframe.onload = function() {
-  if (Features.sidebar) {
-    Sidebar.windowDocument = document;
-    var sidebar = document.getElementById('sidebar');
-    if (sidebar === null) {
-      Sidebar.setUp();
-    }
+  Sidebar.windowDocument = document;
+  var sidebar = document.getElementById('sidebar');
+  if (sidebar === null) {
+    Sidebar.setUp();
   }
 
   EscribaHelper.iframeDocument = iframe.contentWindow.document;
